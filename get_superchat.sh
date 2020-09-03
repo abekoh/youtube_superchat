744fe679cb9cb84a346caedd87e2f381c1b6277d
Kotaro Abe <abekoh.bcky@gmail.com>
2020-09-04 00:15:09 +0900

    update

---
+++ get_superchat.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# YOUTUBE_API_TOKENにアクセストークンつけておく
+VIDEO_ID="DZ4TMHMQzio"
+
+LIVE_CHAT_ID=`curl -X GET -H "Authorization:Bearer ${YOUTUBE_API_TOKEN}" "https://www.googleapis.com/youtube/v3/videos?part=liveStreamingDetails&id=${VIDEO_ID}" | jq -r .items[0].liveStreamingDetails.activeLiveChatId`
+
+echo $LIVE_CHAT_ID
+
+curl -X GET -H "Authorization:Bearer ${YOUTUBE_API_TOKEN}" "https://www.googleapis.com/youtube/v3/liveChat/messages?liveChatId=${LIVE_CHAT_ID}&part=id,snippet&hl=ja"
