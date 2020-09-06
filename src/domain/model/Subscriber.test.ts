36a377f0903291a8168602349cc7d0554332a4c7
Kotaro Abe <abekoh.bcky@gmail.com>
2020-09-06 13:41:22 +0900

    update

---
+++ src/domain/model/Subscriber.test.ts
@@ -0,0 +1,35 @@
+import { IYouTubeClient, LiveChatMessageReponse, LiveChatMessageRequest } from './IYouTubeClient'
+import { Subscriber } from './Subscriber'
+
+class SampleYouTubeClient implements IYouTubeClient {
+  async fetchComments(
+    request: LiveChatMessageRequest
+  ): Promise<LiveChatMessageReponse> {
+    return { messages: [], pageToken: '' }
+  }
+  async getLiveChatIdFromVideoId(videoId: string): Promise<string | null> {
+    return ''
+  }
+}
+
+describe('register', () => {
+  test('returns correct liveChatId', async () => {
+    const youTubeClientSpy = jest
+      .spyOn(SampleYouTubeClient.prototype, 'getLiveChatIdFromVideoId')
+      .mockReturnValue(Promise.resolve('liveChatId'))
+    const subscriber = new Subscriber(new SampleYouTubeClient())
+    const actual = await subscriber.register('videoId')
+    expect(youTubeClientSpy).toHaveBeenCalled()
+    expect(actual).toBe(true)
+  })
+
+  test('returns invalid liveChatId', async () => {
+    const youTubeClientSpy = jest
+      .spyOn(SampleYouTubeClient.prototype, 'getLiveChatIdFromVideoId')
+      .mockReturnValue(Promise.resolve(null))
+    const subscriber = new Subscriber(new SampleYouTubeClient())
+    const actual = await subscriber.register('videoId')
+    expect(youTubeClientSpy).toHaveBeenCalled()
+    expect(actual).toBe(false)
+  })
+})
